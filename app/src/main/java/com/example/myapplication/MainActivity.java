2dcdb995365e089fb473800d1de65a2d66b9ca37
Nail Shamsudinov <nail.shamsudinov@gmail.com>
2023-05-14 15:30:26 +0400

    FirstCommit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Absolute Layout (deprecated)
+        // Linear Layout
+        // Relative Layout
+        // Grid Layout
+        // Constraint Layout (new)
+        // Frame Layout
+    }
+}
